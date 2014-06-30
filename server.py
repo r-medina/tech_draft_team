4bae7f2490cc94156b97dc34e1733b5530a0562d
Michael Probber <michael.probber@hulu.com>
2014-06-30 15:17:28 -0700

    final commit

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 from flask import Flask, request, make_response
 app = Flask(__name__)
+import twitter
+import twitter2
+from website import website_sentiment
 
 @app.route("/", methods = ['POST'])
 def get_data():
 	# Get information in request.form
-	return make_response('Hello World', 200)
+  string = ""
+  string += twitter2.getStatuses(request.form['twitter'])
+  string += website_sentiment(request.form['website'])
+  return make_response(string, 200)
 
 if __name__ == '__main__':
 	app.run(port=8000, debug=True)
