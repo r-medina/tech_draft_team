d5c602fb47ca0c7e561edaa0c5c5cb30d9eafa1f
Srinivasan Vijayaraghavan <srinivav@andrew.cmu.edu>
2014-06-30 15:12:23 -0700

    Twitter sentiment

--- twitter-user.py
+++ twitter-user.py
@@ -1,4 +1,4 @@
-from twitter import Twitter, OAuth, TwitterHTTPError
+import twitter
 import json, sentiment_analysis
 
 OAUTH_TOKEN = '6015802-ja2jaDc8ZFlRfH7CQKRKknuLjGOs6L7SafJNgfIu5Q'
@@ -6,13 +6,15 @@ OAUTH_SECRET = 'v8NwzfbbBmxE2F20eP0XWg9x7ePfyKIuTFMrFo4E4jOd0'
 CONSUMER_KEY = '9AmgHI6GRXuffVmPF1xsCBquF'
 CONSUMER_SECRET = 'xtVNoWtwEprLkxEJQt6nxNhOOy06Mf31yIsxahZTzJ4D0cvsZm'
 
-t = Twitter(auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET,
-            CONSUMER_KEY, CONSUMER_SECRET))
+t = twitter.Api(consumer_key=CONSUMER_KEY,
+    consumer_secret=CONSUMER_SECRET,
+    access_token_key=OAUTH_TOKEN,
+    access_token_secret=OAUTH_SECRET)
 
 def getStatuses(username):
 	statuses = t.GetUserTimeline(username)
 	texts = [s.text for s in statuses]
-        combinedText = texts.join("")
+        combinedText = "".join(texts)
         return combinedText
 
-get_sentiment(getStatuses(username='mumbaikara'))
+print sentiment_analysis.get_sentiment(getStatuses(username='mumbaikara'))
